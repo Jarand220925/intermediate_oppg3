a8b52c5fa990e014413302966564f47558e57fc2
Jarand220925 <jarandkurs@gmail.com>
2025-11-26 10:38:58 +0100

    første

---
+++ intermediate_oppg3/Program.cs
@@ -0,0 +1,88 @@
+﻿using intermediate_oppg3.functions;
+
+namespace intermediate_oppg3;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        //await CountWithAsyncAwait();
+        await AsyncSample.Count();
+        AsyncSample.WriteSample();
+        Console.WriteLine("I'm the last WriteLine.");
+    }
+    
+    
+    public class CounterModel(string name, int maxCount, int delay)
+
+    {
+
+        public string Name {get;set;} = name;
+
+        public int MaxCount {get;set;} = maxCount;
+
+        public int DelayMs {get;set;} = delay;
+
+    }
+
+
+
+    public static async Task CountWithAsyncAwait()
+
+    {
+
+        Console.WriteLine("Starting counter on Async Task...");
+
+
+
+        var counter1 = new CounterModel("Async Counter 1", 5, 500);
+
+        var counter2 = new CounterModel("Async Counter 2", 6, 300);
+
+
+
+        var task1 = AsyncCounter(counter1);
+
+        var task2 = AsyncCounter(counter2);
+
+
+
+        // Venter på at begge oppgavene skal fullføres
+
+        await Task.WhenAll(task1, task2);
+
+    
+
+        Console.WriteLine("Async Tasks completed!");
+
+    }
+
+
+
+    private static async Task AsyncCounter(CounterModel model)
+
+    {
+
+        Console.WriteLine($"Async Task {model.Name} starting..");
+
+    
+
+        for (int i = 0; i <= model.MaxCount; i++)
+
+        {
+
+            await Task.Delay(model.DelayMs);
+
+            Console.WriteLine($"Async Task {model.Name} has counted {i} / {model.MaxCount}...");
+
+        }
+
+
+
+        Console.WriteLine($"Async Task {model.Name} has completed the count...");
+
+    }
+
+
+}
---
+++ intermediate_oppg3/functions/AsyncSample.cs
@@ -0,0 +1,29 @@
+﻿namespace intermediate_oppg3.functions;
+
+public static class AsyncSample
+{
+    private static int _runTimerExCount;
+    public static async Task Count()
+    {
+        await _runTimer();
+        Console.WriteLine("Count is done, World!");
+        WriteSample();
+    }
+
+    public static async Task _runTimer()
+    {
+        for (int i = 0; i < 11; i++)
+        {
+            await Task.Delay(250);
+            Console.WriteLine(i);
+        }
+        _runTimerExCount++;
+        Console.WriteLine($"{_runTimerExCount} times executed _runTimer");
+        WriteSample();
+    }
+
+    public static async void WriteSample()
+    {
+        Console.WriteLine("WriteSample");
+    }
+}
